2f7e5952f5e90549ead2b2ae29b2607c6a6c5164
JakeBrowning90 <jacobbrowning90@gmail.com>
2026-06-22 15:27:28 -0400

    Add conditional value to cellsPerSec

--- src/modules/gameVariables.js
+++ src/modules/gameVariables.js
@@ -20,9 +20,20 @@ let fieldSize = 15;
 let bodyLength = 3;
 
 // Movement speed
-let speedInput = 8;
-let cellsPerSec = speedInput * 0.1;
-// let cellsPerSec = 0.25;
+// let speedInput = 8;
+// let cellsPerSec = speedInput * 0.1;
+let selection = "fast";
+// let selection = "medium";
+// let selection = "slow";
+
+let cellsPerSec;
+if (selection == "fast") {
+  cellsPerSec = 0.8;
+} else if (selection == "medium") {
+  cellsPerSec = 0.6;
+} else if (selection == "slow") {
+  cellsPerSec = 0.4;
+}
 
 let speed = (1 - cellsPerSec) * 1000;
 
